d11a7e0678fdf3a7371463bf30283f9f5b6aa45a
KillianG <killian@airbuds.fm>
2026-04-20 17:37:51 +0200

    Update Spoonlift to 0.0.5

--- homebrew-tap/Casks/spoonlift.rb
+++ homebrew-tap/Casks/spoonlift.rb
@@ -1,12 +1,12 @@
 cask "spoonlift" do
   version "0.0.5"
-  sha256 "REPLACE_WITH_DMG_SHA256"
+  sha256 "53625ed12db7bdc8876468b1feb9d271dfc87bdea9d2692c15b73d9c6e5caaa3"
 
-  url "https://github.com/KillianG/open-forklift/releases/download/v#{version}/Spoonlift-#{version}.dmg",
-      verified: "github.com/KillianG/open-forklift/"
+  url "https://github.com/KillianG/Spoonlift/releases/download/v#{version}/Spoonlift-#{version}.dmg",
+      verified: "github.com/KillianG/Spoonlift/"
   name "Spoonlift"
   desc "Dual-pane file manager inspired by Forklift"
-  homepage "https://github.com/KillianG/open-forklift"
+  homepage "https://github.com/KillianG/Spoonlift"
 
   livecheck do
     url :url
